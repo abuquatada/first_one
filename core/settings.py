fe45053bedd8d386fde26ddda498eb7566ec7edc
Abu Quatada <abuquatada84@gmail.com>
2024-02-02 18:53:53 +0530

    ignore

--- core/settings.py
+++ core/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "new1",
-        "USER": "root",
-        "PASSWORD": "@Abu786",
-        "HOST": "localhost",
-        "PORT": "3306",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "new1",
+#         "USER": "root",
+#         "PASSWORD": "@Abu786",
+#         "HOST": "localhost",
+#         "PORT": "3306",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
